e2d5b3d16c1a8673e8f18e8110f85ab045295ed5
varsha.ryali <varsha.ryali@newfold.com>
2025-07-02 22:12:12 +0530

    feat: prepare for GitHub Packages publishing
    - Update package.json with scoped name @varsha.ryali/changelog-pro
    - Add publishConfig for GitHub Packages registry
    - Create .npmrc for scoped registry configuration
    - Add Jest tests for package validation
    - Clean up template and renderer to match reference design

---
+++ test/basic.test.js
@@ -0,0 +1,26 @@
+/**
+ * Basic Jest tests for Changelog Pro
+ */
+
+describe('Changelog Pro', () => {
+  test('module can be imported', () => {
+    const { ChangelogService, generate, getStats } = require('../index');
+    
+    expect(typeof ChangelogService).toBe('function');
+    expect(typeof generate).toBe('function');
+    expect(typeof getStats).toBe('function');
+  });
+
+  test('ChangelogService can be instantiated', () => {
+    const { ChangelogService } = require('../index');
+    const service = new ChangelogService({ config: {} });
+    
+    expect(service).toBeInstanceOf(ChangelogService);
+  });
+
+  test('HTML renderer exists', () => {
+    const { HtmlRenderer } = require('../lib/html-renderer');
+    
+    expect(typeof HtmlRenderer).toBe('function');
+  });
+}); 
